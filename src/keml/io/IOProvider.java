355a4e55977c74ae6c0f008edf61cf77d8b7ad0c
Noah Heuser <nheuser@uni-koblenz.de>
2025-01-13 18:55:00 +0100

    initial commit

--- src/keml/io/IOProvider.java
+++ src/keml/io/IOProvider.java
@@ -1,3 +1,4 @@
+
 /**
  * 
  */
@@ -17,63 +18,95 @@ import keml.io.llm.ConversationAdder;
  * 
  */
 public class IOProvider {
-	
 
-	/**
-	 * @param args
-	 * @throws IOException 
-	 */
-	public static void main(String[] args) throws Exception {
-		
-		String folder;
-		if (args.length == 0) {
-			folder = "../keml.sample/introductoryExamples";
-		} else {
-			folder = args[0];			
+	String defaultFolder;
+	String folder;
+
+	IOProvider(String defaultFolder) {
+		this.defaultFolder = defaultFolder;
+	}
+
+	private void runIOProvider() {
+		folder = "";
+		FileBrowseDialog fb = new FileBrowseDialog(this);
+		new Thread(fb).start();
+		boolean foundFolder = false;
+		File resultsFolder = null;
+		String conversationFolder = "";
+		File files[] = null;
+		while (!foundFolder) {
+			synchronized (this) {
+				try {
+					while (folder.isEmpty()) {
+						this.wait();
+					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			resultsFolder = new File(folder + "/keml/");
+			System.out.println("You started the graphml to keml conversion.\n I will read graphml files from " + folder
+					+ ".\n I will write the resulting files into " + resultsFolder);
+
+			String conversations = folder + "/conversations.json";
+			conversationFolder = folder + "/conv/";
+			try {
+				new ChatGPTReader().split(conversations, conversationFolder);
+				files = new File(folder + "/graphml/")
+						.listFiles((dir, name) -> name.toLowerCase().endsWith(".graphml"));
+				if (files == null) {
+					throw new NullPointerException();
+				}
+				foundFolder = true;
+				fb.dispose();
+			} catch (IOException e) {
+				fb.message.setText(
+						"<html><body>Cannot split<br>" + conversations + "<br>Choose another folder</body></html>");
+				folder = "";
+				fb.setVisible(true);
+			} catch (NullPointerException e) {
+				fb.message.setText("<html><body>No .graphml files found in given folder<br>" + folder
+						+ "<br>Choose another folder</body></html>");
+				folder = "";
+				fb.setVisible(true);
+			}
 		}
-		
-		File resultsFolder = new File(folder + "/keml/");
 
-		System.out.println("You started the graphml to keml conversion.\n I will read graphml files from " + folder 
-				+ ".\n I will write the resulting files into " + resultsFolder);
-
-		String conversations = folder + "/conversations.json";
-		String conversationFolder = folder+"/conv/";
-		try{
-			new ChatGPTReader().split(conversations, conversationFolder);
-		} catch(IOException e) {
-			System.err.println("Cannot split " + conversations);
-			System.err.println(e);
-		}
-		
-		File[] files = new File(folder+"/graphml/").listFiles((dir, name) -> name.toLowerCase().endsWith(".graphml"));
-		
 		KemlFileHandler fileHandler = new KemlFileHandler();
-		
-		for (File file: files) {
-			try {	
+
+		for (File file : files) {
+			try {
 				transformFile(file, fileHandler, resultsFolder, getConvFileFromFile(file, conversationFolder));
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-
 	}
 
-	private static void transformFile(File graphmlPath, KemlFileHandler fileHandler, File targetFolder, File originalConv) throws Exception {
+	private static void transformFile(File graphmlPath, KemlFileHandler fileHandler, File targetFolder,
+			File originalConv) throws Exception {
 		Conversation conv = new GraphML2KEML().readFromPath(graphmlPath.getAbsolutePath());
 		ConversationAdder.addOriginalConv(conv, originalConv);
-		String kemlBasePath = targetFolder +"/" + FilenameUtils.removeExtension(graphmlPath.getName());
+		String kemlBasePath = targetFolder + "/" + FilenameUtils.removeExtension(graphmlPath.getName());
 		String kemlPath = kemlBasePath + ".keml";
 		fileHandler.saveKeml(conv, kemlPath);
-		System.out.println("Saved file as "+kemlPath);
+		System.out.println("Saved file as " + kemlPath);
 		String kemlJSONPath = kemlBasePath + "-keml.json";
 		fileHandler.saveKemlJSON(conv, kemlJSONPath);
-		System.out.println("Saved file as "+kemlJSONPath);
+		System.out.println("Saved file as " + kemlJSONPath);
 	}
-	
+
 	private static File getConvFileFromFile(File file, String conversationFolder) {
-		return new File(conversationFolder + FilenameUtils.getBaseName(file.getPath())+".json");
+		return new File(conversationFolder + FilenameUtils.getBaseName(file.getPath()) + ".json");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String defaultPath = "../keml.sample/introductoryExamples";
+		IOProvider ioProvider = new IOProvider(defaultPath);
+		ioProvider.runIOProvider();
 	}
 
 }
